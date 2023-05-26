198879d45ff424a0a6bde8e719535d7ab2c39b31
rafi <r.rafi1615005@gmail.com>
2023-05-26 09:46:17 +0600

    connected-to-db

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = 9000 || process.env.PORT 
 
 app.use(express.json());
+
+const mongoURI = 'mongodb://127.0.0.1:27017/omni-express';
+mongoose.connect(mongoURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+.then(() => {  console.log("connected to mongoDB")})
+.catch((error:any) => {  console.log(error)})
+    
 
 app.listen(PORT, () => {  
     console.log("listening on port", PORT)
